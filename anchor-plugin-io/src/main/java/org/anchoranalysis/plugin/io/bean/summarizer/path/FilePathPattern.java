05ba9fd3a20f7aee869c43bd0edeb41a391902cb
Owen Feehan <owenfeehan@users.noreply.github.com>
2020-02-15 10:45:32 +0100

    Changed from pluginRepositories to repositories in anchor-plugin-io; removed stray import

--- anchor-plugin-io/src/main/java/org/anchoranalysis/plugin/io/bean/summarizer/path/FilePathPattern.java
+++ anchor-plugin-io/src/main/java/org/anchoranalysis/plugin/io/bean/summarizer/path/FilePathPattern.java
@@ -29,7 +29,6 @@ package org.anchoranalysis.plugin.io.bean.summarizer.path;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
