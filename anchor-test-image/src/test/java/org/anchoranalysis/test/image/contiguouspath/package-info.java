9b3455ea48a64608acfcd309ad9a6d2e92ad1afa
Owen Feehan <owenfeehan@users.noreply.github.com>
2020-02-18 14:01:36 +0100

    anchoranalysis/anchor-assembly#7 added javadoc comment and licenses

---
+++ anchor-test-image/src/test/java/org/anchoranalysis/test/image/contiguouspath/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * The test is located here rather than in anchor-image as no image IO related loading
+ *  classes are yet available in the dependency tree
+ */
+package org.anchoranalysis.test.image.contiguouspath;
