0b4f11dbd2dcaf74d7662100710bf82ca65b3fee
Owen Feehan <owenfeehan@users.noreply.github.com>
2020-03-02 18:11:16 +0100

    #16 ommitted accidentally from previous commit

---
+++ anchor-plugin-io/src/main/java/org/anchoranalysis/plugin/io/bean/rasterreader/ReadVoxelExtentXml.java
@@ -0,0 +1,118 @@
+package org.anchoranalysis.plugin.io.bean.rasterreader;
+
+/*-
+ * #%L
+ * anchor-plugin-io
+ * %%
+ * Copyright (C) 2010 - 2019 Owen Feehan, ETH Zurich, University of Zurich, Hoffmann la Roche
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
+import java.io.File;
+import java.nio.file.Path;
+
+import org.anchoranalysis.bean.annotation.BeanField;
+import org.anchoranalysis.image.extent.ImageDim;
+import org.anchoranalysis.image.extent.ImageRes;
+import org.anchoranalysis.image.io.RasterIOException;
+import org.anchoranalysis.image.io.bean.rasterreader.RasterReader;
+import org.anchoranalysis.image.io.rasterreader.OpenedRaster;
+import org.anchoranalysis.plugin.io.xml.AnchorMetadataXml;
+
+public class ReadVoxelExtentXml extends RasterReader {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	// START BEAN PROPERTIES
+	@BeanField
+	private RasterReader rasterReader;
+	
+	@BeanField
+	private boolean acceptNoResolution = true;
+	// END BEAN PROPERTIES
+
+	
+	/**
+	 * Looks for a metadata file describing the resolution
+	 * 
+	 * Given an existing image filepath, the filePath.xml is checked
+	 * e.g. given /somePath/rasterReader.tif it will look for /somePath/RasterRader.tif.xml  
+	 * 
+	 * @param filepath the filepath of the image
+	 * @param acceptNoResolution
+	 * @return the scene res if the metadata file exists and was parsed. null otherwise.
+	 * @throws RasterIOException
+	 */
+	public static ImageRes readMetadata( Path filepath, boolean acceptNoResolution ) throws RasterIOException {
+		
+		// How we try to open the metadata
+		ImageRes res = null;
+		File fileMeta = new File( filepath.toString() + ".xml" );
+		  
+		if( fileMeta.exists() ) {
+			res = AnchorMetadataXml.readResolutionXml( fileMeta );	    	  
+		} else {
+			if (acceptNoResolution==false) {
+				throw new RasterIOException( String.format("Resolution metadata is required for '%s'", filepath) );
+			}
+		}
+		return res;
+	}
+		
+	private static void replaceResIfNotNull( ImageDim dims, ImageRes res ) {
+		if (res!=null) {
+			dims.setRes(res);
+		}
+	}
+	
+	@Override
+	public OpenedRaster openFile(Path filepath) throws RasterIOException {
+		
+		OpenedRaster delegate = rasterReader.openFile(filepath);
+		
+		ImageRes sr = readMetadata(filepath, acceptNoResolution);
+		
+		return new OpenedRasterAlterDimensions(
+			delegate,
+			(ImageDim sd) -> replaceResIfNotNull(sd,sr)
+		);
+	}
+
+	public RasterReader getRasterReader() {
+		return rasterReader;
+	}
+
+	public void setRasterReader(RasterReader rasterReader) {
+		this.rasterReader = rasterReader;
+	}
+
+	public boolean isAcceptNoResolution() {
+		return acceptNoResolution;
+	}
+
+	public void setAcceptNoResolution(boolean acceptNoResolution) {
+		this.acceptNoResolution = acceptNoResolution;
+	}
+
+}
