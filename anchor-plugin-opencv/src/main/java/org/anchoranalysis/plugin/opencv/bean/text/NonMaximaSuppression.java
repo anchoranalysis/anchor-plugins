1a718b6f24d267201eb10a32cb833e4179d139b3
Owen Feehan <owenfeehan@users.noreply.github.com>
2020-03-31 04:11:24 +0200

    anchoranalysis/anchor-plugins#21 ObjMaskProviderExtractText.java now uses non-axis-aligned bounding boxes.

--- anchor-plugin-opencv/src/main/java/org/anchoranalysis/plugin/opencv/bean/text/NonMaximaSuppression.java
+++ anchor-plugin-opencv/src/main/java/org/anchoranalysis/plugin/opencv/bean/text/NonMaximaSuppression.java
@@ -31,6 +31,7 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.PriorityQueue;
+import java.util.function.BiFunction;
 
 
 /**
@@ -38,14 +39,28 @@ import java.util.PriorityQueue;
  * 
  * <ol>
  * <li>Select highest-confidence proposal, remove from list, add to output</li>
- * <li>Compare this proposal with remainder, and remove any remainder with overlap above a threshold</li>
+ * <li>Compare this proposal with remainder, and remove any remainder with a similarity score above a threshold</li>
  * <li>If there are remaining proposals in the queue, goto Step 1</li>
  * </ol>
+ * 
+ * @param T the type-of-object to which the algorithm applies
  * 
  * @author owen
  *
  */
-class NonMaximaSuppression {
+class NonMaximaSuppression<T> {
+
+	private BiFunction<T,T,Double> funcScore;
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param funcScore measures a similarity (e.g. IntersectionOverUnion between two objects)
+	 */
+	public NonMaximaSuppression(BiFunction<T, T, Double> funcScore) {
+		super();
+		this.funcScore = funcScore;
+	}
 
 	/**
 	 * Reduce a set of bounding-boxes (each with a confidence score) to a set with minimal overlap.
@@ -56,17 +71,19 @@ class NonMaximaSuppression {
 	 * @param overlapThreshold after a proposal is accepted, other proposals with overlap greater-equal than this threshold are removed
 	 * @return accepted proposals
 	 */
-	public static List<BoundingBoxWithConfidence> apply( Collection<BoundingBoxWithConfidence> proposals, double overlapThreshold ) {
+	public List<WithConfidence<T>> apply(
+		Collection<WithConfidence<T>> proposals,
+		double overlapThreshold
+	) {
+		PriorityQueue<WithConfidence<T>> pq = new PriorityQueue<>(proposals);
 		
-		PriorityQueue<BoundingBoxWithConfidence> pq = new PriorityQueue<>(proposals);
-		
-		List<BoundingBoxWithConfidence> out = new ArrayList<>();
+		List<WithConfidence<T>> out = new ArrayList<>();
 		
 		while( !pq.isEmpty() ) {
 			
-			BoundingBoxWithConfidence highestConfidence = pq.poll();
+			WithConfidence<T> highestConfidence = pq.poll();
 			
-			removeOverlapping( highestConfidence, pq, overlapThreshold);
+			removeSimilar( highestConfidence, pq, overlapThreshold);
 			
 			out.add(highestConfidence);
 		}
@@ -74,16 +91,17 @@ class NonMaximaSuppression {
 		return out;
 	}
 	
-	private static void removeOverlapping( BoundingBoxWithConfidence proposal, PriorityQueue<BoundingBoxWithConfidence> others, double overlapThreshold ) {
+	private void removeSimilar( WithConfidence<T> proposal, PriorityQueue<WithConfidence<T>> others, double scoreThreshold ) {
 		
-		Iterator<BoundingBoxWithConfidence> itr = others.iterator();
+		Iterator<WithConfidence<T>> itr = others.iterator();
 		while( itr.hasNext() ) {
 			
-			BoundingBoxWithConfidence bbox = itr.next();
+			WithConfidence<T> bbox = itr.next();
 			
-			double overlap = proposal.intersectionOverUnion(bbox.getBBox());
+			double score = funcScore.apply(proposal.getObj(), bbox.getObj());
 			
-			if (overlap >= overlapThreshold) {
+			// These objects are deemed highly-similar and removed
+			if (score >= scoreThreshold) {
 				// Remove from the queue if above the threshold
 				itr.remove();
 			}
--- anchor-plugin-opencv/src/main/java/org/anchoranalysis/plugin/opencv/bean/text/ObjMaskProviderExtractText.java
+++ anchor-plugin-opencv/src/main/java/org/anchoranalysis/plugin/opencv/bean/text/ObjMaskProviderExtractText.java
@@ -2,8 +2,11 @@ package org.anchoranalysis.plugin.opencv.bean.text;
 
 import java.nio.file.Path;
 import java.util.List;
+import java.util.stream.Collectors;
 
-
+import org.anchoranalysis.anchor.mpp.mark.GlobalRegionIdentifiers;
+import org.anchoranalysis.anchor.mpp.mark.Mark;
+import org.anchoranalysis.anchor.mpp.regionmap.RegionMapSingleton;
 
 /*-
  * #%L
@@ -36,8 +39,15 @@ import org.anchoranalysis.core.error.CreateException;
 import org.anchoranalysis.core.error.OperationFailedException;
 import org.anchoranalysis.image.bean.provider.ObjMaskProvider;
 import org.anchoranalysis.image.bean.provider.stack.StackProvider;
+import org.anchoranalysis.image.binary.values.BinaryValuesByte;
+import org.anchoranalysis.image.extent.BoundingBox;
 import org.anchoranalysis.image.extent.Extent;
+import org.anchoranalysis.image.extent.ImageDim;
+import org.anchoranalysis.image.extent.ImageRes;
+import org.anchoranalysis.image.interpolator.InterpolatorFactory;
+import org.anchoranalysis.image.objmask.ObjMask;
 import org.anchoranalysis.image.objmask.ObjMaskCollection;
+import org.anchoranalysis.image.objmask.properties.ObjMaskWithProperties;
 import org.anchoranalysis.image.scale.ScaleFactor;
 import org.anchoranalysis.image.stack.Stack;
 import org.anchoranalysis.plugin.opencv.CVInit;
@@ -92,9 +102,13 @@ public class ObjMaskProviderExtractText extends ObjMaskProvider {
 	 */
 	private static final int MAX_SCALE_FACTOR = (720/EAST_EXTENT.getY()) + 1;
 	
+	private NonMaximaSuppression<BoundingBox> nonMaximaSuppression = new NonMaximaSuppression<>(
+		ObjMaskProviderExtractText::intersectionOverUnion
+	);
+	
 	@Override
 	public ObjMaskCollection create() throws CreateException {
-
+		
 		Stack stack = createInput();
 		
 		Extent extent;
@@ -110,18 +124,70 @@ public class ObjMaskProviderExtractText extends ObjMaskProvider {
 		
 		Pair<Mat,ScaleFactor> pair = CreateScaledInput.apply(stack, extent);
 		
-		List<BoundingBoxWithConfidence> listBoxes = EastBoundingBoxExtractor.extractBoundingBoxes(
+		List<WithConfidence<Mark>> listMarks = EastBoundingBoxExtractor.extractBoundingBoxes(
 			pair.getFirst(),
 			minConfidence,
-			pathToEastModel(),
-			stack.getDimensions().getRes()
+			pathToEastModel()
 		);
 		
+		List<WithConfidence<ObjMask>> listBoxes = convertToObjMask(
+			listMarks,
+			dimsForMat(pair.getFirst(), stack.getDimensions().getRes() )
+		);
+		
+		listBoxes = maybeFilterList(listBoxes);
+		
 		// Scale each bounding box back to the original-size, and convert into an object-mask
-		return ScaleConvertToMask.scaledMasksForEachBox(
+		/*return ScaleConvertToMask.scaledMasksForEachBox(
 			maybeFilterList(listBoxes),
 			pair.getSecond(),
 			stack.getDimensions()
+		);*/
+		ObjMaskCollection objs = new ObjMaskCollection(
+			listBoxes.stream().map(wc->wc.getObj()).collect( Collectors.toList() )
+		);
+		try {
+			objs.scale(pair.getSecond(), InterpolatorFactory.getInstance().binaryResizing() );
+		} catch (OperationFailedException e) {
+			assert(false);
+		}
+		
+		return objs;
+	}
+	
+	private ImageDim dimsForMat( Mat mat, ImageRes res ) {
+		
+		int width = (int) mat.size().width;
+		int height = (int) mat.size().height;
+		
+		ImageDim dims = new ImageDim(
+			new Extent(width, height, 1),
+			res
+		);
+		
+		return dims;
+	}
+	
+	private static List<WithConfidence<ObjMask>> convertToObjMask(
+		List<WithConfidence<Mark>> listMarks,
+		ImageDim dim
+	) {
+		return listMarks.stream()
+				.map( wc -> convertToObjMask(wc, dim) )
+				.collect( Collectors.toList() );
+	}
+	
+	private static WithConfidence<ObjMask> convertToObjMask( WithConfidence<Mark> wcMark, ImageDim dim ) {
+
+		
+		ObjMaskWithProperties om = wcMark.getObj().calcMask(
+			dim,
+			RegionMapSingleton.instance().membershipWithFlagsForIndex(GlobalRegionIdentifiers.SUBMARK_INSIDE),
+			BinaryValuesByte.getDefault()
+		);
+		return new WithConfidence<ObjMask>(
+			om.getMask(),
+			wcMark.getConfidence()
 		);
 	}
 	
@@ -146,16 +212,77 @@ public class ObjMaskProviderExtractText extends ObjMaskProvider {
 	}
 
 	/** Maybe apply non-maxima suppression */
-	private List<BoundingBoxWithConfidence> maybeFilterList( List<BoundingBoxWithConfidence> listBoxes ) {
+	/*private List<WithConfidence<BoundingBox>> maybeFilterList( List<WithConfidence<BoundingBox>> listBoxes ) {
 		if (suppressNonMaxima) {
-			return NonMaximaSuppression.apply(
+			return nonMaximaSuppression.apply(
 				listBoxes,
 				suppressIntersectionOverUnionAbove
 			);
 		} else {
 			return listBoxes;
 		}
+	}*/
+	
+	private List<WithConfidence<ObjMask>> maybeFilterList( List<WithConfidence<ObjMask>> listBoxes ) {
+		if (suppressNonMaxima) {
+			/*PriorityQueue<WithConfidence<ObjMask>> pq = new PriorityQueue<>(listBoxes);
+			return Arrays.asList( pq.remove() );*/
+			/*return nonMaximaSuppression.apply(
+				listBoxes,
+				suppressIntersectionOverUnionAbove
+			);*/
+			return listBoxes;
+		} else {
+			return listBoxes;
+		}
 	}
+	
+	/**
+	 * The Intersection over Union (IoU) metric for two bounding-boxes.
+	 * 
+	 * @see <a href="https://www.quora.com/How-does-non-maximum-suppression-work-in-object-detection">Intersection-over-Union</a>
+	 * @param second the other bounding-box to consider when calculating the IoU
+	 * @return the IoU score
+	 */
+	private static double intersectionOverUnion(BoundingBox first, BoundingBox second) {
+		
+		BoundingBox intersection = new BoundingBox(first);
+		if (!intersection.intersect(second, true)) {
+			// If there's no intersection then the score is 0
+			return 0.0;
+		}
+		
+		int intersectionArea = intersection.extnt().getVolume();
+		
+		// The total area is equal to the sum of both minus the intersection (which is otherwise counted twice)
+		int total = second.extnt().getVolume() + first.extnt().getVolume() - intersectionArea;
+		
+		return ((double) intersectionArea) / total;
+	}
+	
+	
+	/**
+	 * The Intersection over Union (IoU) metric for two bounding-boxes.
+	 * 
+	 * @see <a href="https://www.quora.com/How-does-non-maximum-suppression-work-in-object-detection">Intersection-over-Union</a>
+	 * @param second the other bounding-box to consider when calculating the IoU
+	 * @return the IoU score
+	 */
+	/*private static double overlapRatio(ObjMaskOverlap first, ObjMaskOverlap second) {
+		
+		BoundingBox intersection = new BoundingBox(first);
+		if (!intersection.intersect(second, true)) {
+			// If there's no intersection then the score is 0
+			return 0.0;
+		}
+		
+		int intersectionArea = intersection.extnt().getVolume();
+		
+		// The total area is equal to the sum of both minus the intersection (which is otherwise counted twice)
+		int total = second.extnt().getVolume() + first.extnt().getVolume() - intersectionArea;
+		
+		return ((double) intersectionArea) / total;
+	}*/
 	
 	private Path pathToEastModel() {
 		return getSharedObjects().getModelDir().resolve("frozen_east_text_detection.pb");
