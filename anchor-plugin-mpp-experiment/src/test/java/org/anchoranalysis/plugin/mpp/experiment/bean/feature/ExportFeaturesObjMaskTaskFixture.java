b0db0097811b5029e329fe693097c8688df9b61d
Owen Feehan <owenfeehan@users.noreply.github.com>
2020-04-27 20:28:00 +0200

    anchoranalysis/anchor#33 refactored ExportFeaturesObjMaskTaskTest

--- anchor-plugin-mpp-experiment/src/test/java/org/anchoranalysis/plugin/mpp/experiment/bean/feature/ExportFeaturesObjMaskTaskFixture.java
+++ anchor-plugin-mpp-experiment/src/test/java/org/anchoranalysis/plugin/mpp/experiment/bean/feature/ExportFeaturesObjMaskTaskFixture.java
@@ -5,8 +5,6 @@ import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
-
 import org.anchoranalysis.bean.NamedBean;
 import org.anchoranalysis.core.error.CreateException;
 import org.anchoranalysis.feature.bean.list.FeatureListProvider;
@@ -19,22 +17,73 @@ import org.anchoranalysis.plugin.mpp.experiment.bean.feature.flexi.FlexiFeatureT
 import org.anchoranalysis.plugin.mpp.experiment.bean.feature.flexi.MergedPairs;
 import org.anchoranalysis.test.TestLoader;
 import org.anchoranalysis.test.feature.plugins.FeaturesFromXmlFixture;
+import org.anchoranalysis.test.image.NRGStackFixture;
+import org.anchoranalysis.plugin.mpp.experiment.bean.feature.flexi.Simple;
 
 import ch.ethz.biol.cell.imageprocessing.objmask.provider.ObjMaskProviderReference;
 
 class ExportFeaturesObjMaskTaskFixture {
 
-	private TestLoader loader;
+	private static final String PATH_FEATURES = "singleFeatures.xml";
+	private static final String PATH_FEATURES_SHELL = "singleFeaturesWithShell.xml";
+	private static final String PATH_FEATURES_PAIR = "pairFeatures.xml";
 	
+	private TestLoader loader;
+	private NRGStack nrgStack = createNRGStack(true);
+	private String pathFeatures = PATH_FEATURES;
+	private FlexiFeatureTable<?> flexiFeatureTable = new Simple();
+	
+	/**
+	 * Constructor
+	 * 
+	 * <p>By default, use a big-sized NRG-stack that functions with our feature-lists</p>
+	 * <p>By default, load the features from PATH_FEATURES</p>
+	 * <p>By default, use Simple feature-mode. It can be changed to Merged-Pairs.</p>
+	 * 
+	 * @param loader
+	 */
 	public ExportFeaturesObjMaskTaskFixture(TestLoader loader) {
 		this.loader = loader;
+		this.nrgStack = createNRGStack(true);
 	}
 	
-	public <T extends FeatureInput> ExportFeaturesObjMaskTask<T> createTask(
-		String pathFeatures,
-		FlexiFeatureTable<T> selectFeaturesObjects,
-		NRGStack nrgStack
-	) throws CreateException {
+	/** Change to using a small nrg-stack that causes some features to throw errors */
+	public void useSmallNRGInstead() {
+		this.nrgStack = createNRGStack(false);
+	}
+	
+	/** Additionally include a shell feature in the "single" features */
+	public void includeAdditionalShellFeature() {
+		this.pathFeatures = PATH_FEATURES_SHELL;
+	}
+	
+	/** 
+	 * Change to use Merged-Pairs mode rather than Simple mode
+	 *
+	 * @param includeFeaturesInPair iff TRUE "pair" features are populated in merged-pair mode
+	 * @throws CreateException 
+	 **/
+	public void changeToMergedPairs(boolean includeFeaturesInPair) throws CreateException {
+		flexiFeatureTable = createMergedPairs(includeFeaturesInPair);
+	}
+	
+	private MergedPairs createMergedPairs(boolean includeFeaturesInPair) throws CreateException {
+		MergedPairs mergedPairs = new MergedPairs();
+		mergedPairs.setSuppressErrors(true);
+		if (includeFeaturesInPair) {
+			mergedPairs.setListFeaturesPair(
+				loadFeatures(loader, PATH_FEATURES_PAIR)
+			);
+		}
+		return mergedPairs;
+	}
+	
+	public NRGStack getNrgStack() {
+		return nrgStack;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public <T extends FeatureInput> ExportFeaturesObjMaskTask<T> createTask() throws CreateException {
 				
 		ExportFeaturesObjMaskTask<T> task = new ExportFeaturesObjMaskTask<>();
 
@@ -44,24 +93,12 @@ class ExportFeaturesObjMaskTaskFixture {
 		task.setNrgStackProvider(
 			nrgStackProvider(nrgStack)
 		);
-		task.setSelectFeaturesObjects(
-			selectFeaturesObjects
-		);
+		
+		task.setSelectFeaturesObjects( (FlexiFeatureTable<T>) flexiFeatureTable);
 		task.setListObjMaskProvider(
 			createObjProviders(MultiInputFixture.OBJS_NAME)
 		);
 		return task;
-	}
-	
-	public MergedPairs createMergedPairs( Optional<String> pathPairFeatures ) throws CreateException {
-		MergedPairs mergedPairs = new MergedPairs();
-		mergedPairs.setSuppressErrors(true);
-		if (pathPairFeatures.isPresent()) {
-			mergedPairs.setListFeaturesPair(
-				loadFeatures(loader, pathPairFeatures.get())
-			);
-		}
-		return mergedPairs;
 	}
 		
 	private static List<NamedBean<ObjMaskProvider>> createObjProviders(String objsName) {
@@ -80,6 +117,10 @@ class ExportFeaturesObjMaskTaskFixture {
 		when(stackProvider.create()).thenReturn(stack);
 		when(stackProvider.duplicateBean()).thenReturn(stackProvider);
 		return stackProvider;
+	}
+	
+	private NRGStack createNRGStack( boolean bigSizeNrg ) {
+		return NRGStackFixture.create(bigSizeNrg, false).getNrgStack();
 	}
 	
 	/** creates a feature-list associated with obj-mask
