05fa474d13d8e9d28fd4a8e4ec0c6adfd54ac081
Owen Feehan <owenfeehan@users.noreply.github.com>
2020-05-06 22:44:37 +0200

    Removing duplication, including deleting some redundant least-squares classes

---
+++ anchor-plugin-operator-feature/src/main/java/org/anchoranalysis/plugin/operator/feature/bean/arithmetic/ConstantValueFeatureGeneric.java
@@ -0,0 +1,55 @@
+package org.anchoranalysis.plugin.operator.feature.bean.arithmetic;
+
+import org.anchoranalysis.bean.annotation.BeanField;
+import org.anchoranalysis.feature.bean.operator.FeatureGenericSingleElem;
+import org.anchoranalysis.feature.cache.SessionInput;
+import org.anchoranalysis.feature.calc.FeatureCalcException;
+import org.anchoranalysis.feature.input.FeatureInput;
+
+/**
+ * Operations that use a specified constant value
+ * 
+ * @author Owen Feehan
+ *
+ * @param <T>
+ */
+public abstract class ConstantValueFeatureGeneric<T extends FeatureInput> extends FeatureGenericSingleElem<T> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	// START BEAN PROPERTIES
+	@BeanField
+	private double value = 0;
+	// END BEAN PROPERTIES
+	
+	@Override
+	public double calc( SessionInput<T> input ) throws FeatureCalcException {
+		return combineValueAndFeature(
+			value,
+			input.calc( getItem() )
+		);
+	}
+	
+	@Override
+	public String getDscrLong() {
+		return combineDscr(
+			String.format("%f", value),
+			getItem().getDscrLong()
+		);
+	}
+	
+	protected abstract double combineValueAndFeature( double value, double featureResult );
+
+	protected abstract String combineDscr( String valueDscr, String featureDscr);
+	
+	public double getValue() {
+		return value;
+	}
+
+	public void setValue(double value) {
+		this.value = value;
+	}
+}
