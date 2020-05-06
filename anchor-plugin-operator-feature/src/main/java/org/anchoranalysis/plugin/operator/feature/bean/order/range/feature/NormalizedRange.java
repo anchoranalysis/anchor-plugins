7ad3ddff7037e66a730276166134729684cc5e70
Owen Feehan <owenfeehan@users.noreply.github.com>
2020-05-06 04:04:20 +0200

    Repackaging feature generic operators

--- anchor-plugin-operator-feature/src/main/java/org/anchoranalysis/plugin/operator/feature/bean/order/range/feature/NormalizedRange.java
+++ anchor-plugin-operator-feature/src/main/java/org/anchoranalysis/plugin/operator/feature/bean/order/range/feature/NormalizedRange.java
@@ -1,10 +1,10 @@
-package ch.ethz.biol.cell.mpp.nrg.feature.operator;
+package org.anchoranalysis.plugin.operator.feature.bean.order.range.feature;
 
-/*
+/*-
  * #%L
- * anchor-feature
+ * anchor-plugin-operator-feature
  * %%
- * Copyright (C) 2016 ETH Zurich, University of Zurich, Owen Feehan
+ * Copyright (C) 2010 - 2019 Owen Feehan, ETH Zurich, University of Zurich, Hoffmann la Roche
  * %%
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -26,14 +26,19 @@ package ch.ethz.biol.cell.mpp.nrg.feature.operator;
  * #L%
  */
 
-
-import org.anchoranalysis.bean.annotation.BeanField;
 import org.anchoranalysis.feature.bean.operator.FeatureDoubleElem;
 import org.anchoranalysis.feature.cache.SessionInput;
 import org.anchoranalysis.feature.calc.FeatureCalcException;
 import org.anchoranalysis.feature.input.FeatureInput;
 
-public class AbsoluteDifference<T extends FeatureInput> extends FeatureDoubleElem<T> {
+/**
+ * The range of two feature values (i.e. max - min), divided by their mean.
+ * 
+ * @author Owen Feehan
+ *
+ * @param <T> feature input-type
+ */
+public class NormalizedRange<T extends FeatureInput> extends FeatureDoubleElem<T> {
 
 	/**
 	 * 
@@ -41,47 +46,25 @@ public class AbsoluteDifference<T extends FeatureInput> extends FeatureDoubleEle
 	private static final long serialVersionUID = 1L;
 
 	// START BEAN PROPERTIES
-	@BeanField
-	private double weightItem1Greater = 1.0;
-	
-	@BeanField
-	private double weightItem2Greater = 1.0;
 	// END BEAN PROPERTIES
 	
 	@Override
 	public double calc( SessionInput<T> input ) throws FeatureCalcException {
-		
-		double val1 = input.calc( getItem1() );
+		double val1 = input.calc( getItem1()  );
 		double val2 = input.calc( getItem2() );
 		
-		if (val1==val2) {
+		double absDiff = Math.abs( val1 - val2 ); 
+		double mean = (val1+val2)/2;
+		
+		if (mean==0.0) {
 			return 0.0;
 		}
-		else if (val1>val2) {
-			return weightItem1Greater * (val1-val2);
-		} else {
-			return weightItem2Greater * (val2-val1);
-		}
+		
+		return absDiff / mean;
 	}
-
+	
 	@Override
 	public String getDscrLong() {
 		return String.format("%s - %s", getItem1().getDscrLong(), getItem2().getDscrLong() );
-	}
-	
-	public double getWeightItem1Greater() {
-		return weightItem1Greater;
-	}
-
-	public void setWeightItem1Greater(double weightItem1Greater) {
-		this.weightItem1Greater = weightItem1Greater;
-	}
-
-	public double getWeightItem2Greater() {
-		return weightItem2Greater;
-	}
-
-	public void setWeightItem2Greater(double weightItem2Greater) {
-		this.weightItem2Greater = weightItem2Greater;
 	}
 }
