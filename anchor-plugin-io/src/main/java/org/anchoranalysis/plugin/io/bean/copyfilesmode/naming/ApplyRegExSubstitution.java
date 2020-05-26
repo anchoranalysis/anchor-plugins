684998c6af29b34a16efe5ab6d1c47e7363bc786
Owen Feehan <owenfeehan@users.noreply.github.com>
2020-05-26 12:02:18 +0200

    Removing duplication in copy-files

--- anchor-plugin-io/src/main/java/org/anchoranalysis/plugin/io/bean/copyfilesmode/naming/ApplyRegExSubstitution.java
+++ anchor-plugin-io/src/main/java/org/anchoranalysis/plugin/io/bean/copyfilesmode/naming/ApplyRegExSubstitution.java
@@ -1,34 +1,8 @@
 package org.anchoranalysis.plugin.io.bean.copyfilesmode.naming;
 
-/*-
- * #%L
- * anchor-plugin-io
- * %%
- * Copyright (C) 2010 - 2019 Owen Feehan, ETH Zurich, University of Zurich, Hoffmann la Roche
- * %%
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- * #L%
- */
-
-import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Optional;
 
 import org.anchoranalysis.bean.annotation.BeanField;
 import org.anchoranalysis.io.error.AnchorIOException;
@@ -39,58 +13,21 @@ import org.anchoranalysis.io.error.AnchorIOException;
  * @author FEEHANO
  *
  */
-public class ApplyRegExSubstitution extends CopyFilesNaming {
+public class ApplyRegExSubstitution extends CopyFilesNamingOneRegEx {
 
 	// START BEAN PROPERTIES
-	@BeanField
-	private CopyFilesNaming copyFilesNaming;
-	
-	@BeanField
-	private String regex;
-	
 	@BeanField
 	private String replacement;
 	// END BEAN PROPERTIES
 	
 	@Override
-	public void beforeCopying(Path destDir, int totalNumFiles) {
-		copyFilesNaming.beforeCopying(destDir, totalNumFiles);
+	protected Optional<Path> destinationPathRelative(Path pathDelegate, String regex) throws AnchorIOException {
+		String pathAfterRegEx = NamingUtilities.convertToString(pathDelegate).replaceAll(regex, replacement);
+		return Optional.of(
+			Paths.get(pathAfterRegEx)
+		);
 	}
-
-	@Override
-	public Path destinationPathRelative(Path sourceDir, Path destDir, File file, int iter) throws AnchorIOException {
-		Path path = copyFilesNaming.destinationPathRelative(sourceDir, destDir, file, iter);
-
-		if (path==null) {
-			return null;
-		}
-		
-		String pathAfterRegEx = NamingUtilities.convertToString(path).replaceAll(regex,replacement);
-		
-		return Paths.get(pathAfterRegEx);
-	}
-
-	@Override
-	public void afterCopying(Path destDir, boolean dummyMode) throws AnchorIOException {
-		copyFilesNaming.afterCopying(destDir, dummyMode);
-	}
-
-	public CopyFilesNaming getCopyFilesNaming() {
-		return copyFilesNaming;
-	}
-
-	public void setCopyFilesNaming(CopyFilesNaming copyFilesNaming) {
-		this.copyFilesNaming = copyFilesNaming;
-	}
-
-	public String getRegex() {
-		return regex;
-	}
-
-	public void setRegex(String regex) {
-		this.regex = regex;
-	}
-
+	
 	public String getReplacement() {
 		return replacement;
 	}
@@ -98,5 +35,4 @@ public class ApplyRegExSubstitution extends CopyFilesNaming {
 	public void setReplacement(String replacement) {
 		this.replacement = replacement;
 	}
-
 }
---
+++ anchor-plugin-io/src/main/java/org/anchoranalysis/plugin/io/bean/copyfilesmode/naming/CopyFilesNamingOneRegEx.java
@@ -0,0 +1,30 @@
+package org.anchoranalysis.plugin.io.bean.copyfilesmode.naming;
+
+import java.nio.file.Path;
+import java.util.Optional;
+
+import org.anchoranalysis.bean.annotation.BeanField;
+import org.anchoranalysis.io.error.AnchorIOException;
+
+public abstract class CopyFilesNamingOneRegEx extends CopyFilesNamingOne {
+
+	// START BEAN PROPERTIES
+	@BeanField
+	private String regex;
+	// END BEAN PROPERTIES
+	
+	@Override
+	public Optional<Path> destinationPathRelative(Path pathDelegate) throws AnchorIOException {
+		return destinationPathRelative(pathDelegate, regex);
+	}
+	
+	protected abstract Optional<Path> destinationPathRelative(Path pathDelegate, String regex) throws AnchorIOException;
+		
+	public String getRegex() {
+		return regex;
+	}
+
+	public void setRegex(String regex) {
+		this.regex = regex;
+	}
+}
