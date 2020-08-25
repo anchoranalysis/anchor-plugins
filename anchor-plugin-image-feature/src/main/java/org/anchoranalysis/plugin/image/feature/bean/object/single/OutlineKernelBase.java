5f57639f565cea6a7f311de57a052aa0e6f28f82
Owen Feehan <owenfeehan@users.noreply.github.com>
2020-08-25 02:10:39 +0200

    Refactored OutlineKernelBase

--- anchor-plugin-image-feature/src/main/java/org/anchoranalysis/plugin/image/feature/bean/object/single/OutlineKernelBase.java
+++ anchor-plugin-image-feature/src/main/java/org/anchoranalysis/plugin/image/feature/bean/object/single/OutlineKernelBase.java
@@ -1,7 +1,13 @@
 package org.anchoranalysis.plugin.image.feature.bean.object.single;
 
 import org.anchoranalysis.bean.annotation.BeanField;
+import org.anchoranalysis.core.functional.function.CheckedSupplier;
+import org.anchoranalysis.feature.cache.SessionInput;
+import org.anchoranalysis.feature.calculate.FeatureCalculationException;
+import org.anchoranalysis.feature.energy.EnergyStack;
 import org.anchoranalysis.image.feature.bean.object.single.FeatureSingleObject;
+import org.anchoranalysis.image.feature.object.input.FeatureInputSingleObject;
+import org.anchoranalysis.image.object.ObjectMask;
 import org.anchoranalysis.image.voxel.kernel.outline.OutlineKernelParameters;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,7 +22,17 @@ public abstract class OutlineKernelBase extends FeatureSingleObject {
     @BeanField @Getter @Setter private boolean do3D = false;
     // END BEAN PROPERTIES
     
-    protected OutlineKernelParameters createParameters() {
+    @Override
+    protected double calculate(SessionInput<FeatureInputSingleObject> input)
+            throws FeatureCalculationException {
+
+        FeatureInputSingleObject inputSessionless = input.get();
+        return calculateWithParameters(inputSessionless.getObject(), createParameters(), inputSessionless::getEnergyStackRequired);
+    }
+    
+    protected abstract double calculateWithParameters(ObjectMask object, OutlineKernelParameters parameters, CheckedSupplier<EnergyStack,FeatureCalculationException> energyStack) throws FeatureCalculationException;
+    
+    private OutlineKernelParameters createParameters() {
         return new OutlineKernelParameters(outsideAtThreshold, do3D, ignoreAtThreshold);
     }
 }
