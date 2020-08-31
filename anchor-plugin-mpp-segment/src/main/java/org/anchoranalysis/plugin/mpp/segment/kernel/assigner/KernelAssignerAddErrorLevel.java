141a489f0e720d13647711ec24f34e44655f6506
Owen Feehan <owenfeehan@users.noreply.github.com>
2020-09-01 01:50:42 +0200

    anchoranalysis/anchor#71 renamed anchor-plugin-ij to anchor-plugin-imagej and anchor-plugin-mpp-sgmn to anchor-plugin-mpp-segment (and repackaged the classes)

---
+++ anchor-plugin-mpp-segment/src/main/java/org/anchoranalysis/plugin/mpp/segment/kernel/assigner/KernelAssignerAddErrorLevel.java
@@ -0,0 +1,61 @@
+/*-
+ * #%L
+ * anchor-plugin-mpp-sgmn
+ * %%
+ * Copyright (C) 2010 - 2020 Owen Feehan, ETH Zurich, University of Zurich, Hoffmann-La Roche
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
+package org.anchoranalysis.plugin.mpp.segment.kernel.assigner;
+
+import org.anchoranalysis.mpp.proposer.error.ErrorNode;
+import org.anchoranalysis.mpp.segment.kernel.KernelAssigner;
+import org.anchoranalysis.mpp.segment.kernel.KernelCalculateEnergyException;
+import org.anchoranalysis.mpp.segment.kernel.proposer.KernelWithIdentifier;
+import org.anchoranalysis.mpp.segment.optscheme.step.OptimizationStep;
+import org.anchoranalysis.mpp.segment.transformer.TransformationContext;
+
+public class KernelAssignerAddErrorLevel<S, T> implements KernelAssigner<S, T> {
+
+    private KernelAssigner<S, T> kernelAssigner;
+    private ErrorNode parentErrorNode;
+
+    public KernelAssignerAddErrorLevel(
+            KernelAssigner<S, T> kernelAssigner, ErrorNode parentErrorNode) {
+        super();
+        this.kernelAssigner = kernelAssigner;
+        this.parentErrorNode = parentErrorNode;
+    }
+
+    @Override
+    public void assignProposal(
+            OptimizationStep<S, T> optStep,
+            TransformationContext context,
+            KernelWithIdentifier<S> kernel)
+            throws KernelCalculateEnergyException {
+
+        // Add a sub-level with the Kernel name in the proposer-failure-description
+        ErrorNode errorNode = parentErrorNode.add(kernel.getKernel().getName());
+
+        // We assign the proposed marks
+        kernelAssigner.assignProposal(optStep, context.replaceError(errorNode), kernel);
+    }
+}
---
+++ anchor-plugin-mpp-segment/src/main/java/org/anchoranalysis/plugin/mpp/segment/optscheme/reporter/ConsoleAggregatedReporter.java
@@ -0,0 +1,104 @@
+/*-
+ * #%L
+ * anchor-plugin-mpp-sgmn
+ * %%
+ * Copyright (C) 2010 - 2020 Owen Feehan, ETH Zurich, University of Zurich, Hoffmann-La Roche
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
+package org.anchoranalysis.plugin.mpp.segment.optscheme.reporter;
+
+import lombok.NoArgsConstructor;
+import org.anchoranalysis.mpp.feature.energy.marks.VoxelizedMarksWithEnergy;
+import org.anchoranalysis.mpp.segment.bean.optscheme.feedback.ReporterAgg;
+import org.anchoranalysis.mpp.segment.optscheme.feedback.OptimizationFeedbackEndParams;
+import org.anchoranalysis.mpp.segment.optscheme.feedback.OptimizationFeedbackInitParams;
+import org.anchoranalysis.mpp.segment.optscheme.feedback.ReporterException;
+import org.anchoranalysis.mpp.segment.optscheme.feedback.aggregate.AggregateReceiver;
+import org.anchoranalysis.mpp.segment.optscheme.feedback.aggregate.Aggregator;
+import org.anchoranalysis.mpp.segment.optscheme.feedback.aggregate.AggregatorException;
+import org.anchoranalysis.mpp.segment.optscheme.step.Reporting;
+import org.apache.commons.lang.time.StopWatch;
+
+@NoArgsConstructor
+public final class ConsoleAggregatedReporter extends ReporterAgg<VoxelizedMarksWithEnergy>
+        implements AggregateReceiver<VoxelizedMarksWithEnergy> {
+
+    private StopWatch timer = null;
+
+    public ConsoleAggregatedReporter(double aggIntervalLog10) {
+        super(aggIntervalLog10);
+    }
+
+    @Override
+    public void aggStart(
+            OptimizationFeedbackInitParams<VoxelizedMarksWithEnergy> initParams, Aggregator agg)
+            throws AggregatorException {
+        // NOTHING TO DO
+    }
+
+    @Override
+    public void reportBegin(OptimizationFeedbackInitParams<VoxelizedMarksWithEnergy> initParams)
+            throws ReporterException {
+        super.reportBegin(initParams);
+        timer = new StopWatch();
+        timer.start();
+    }
+
+    @Override
+    public void aggReport(Reporting<VoxelizedMarksWithEnergy> reporting, Aggregator agg) {
+        System.out.printf( // NOSONAR
+                "itr=%d  time=%e  tpi=%e   %s%n",
+                reporting.getIter(),
+                ((double) timer.getTime()) / 1000,
+                ((double) timer.getTime()) / (reporting.getIter() * 1000),
+                agg.toString());
+    }
+
+    @Override
+    public void reportNewBest(Reporting<VoxelizedMarksWithEnergy> reporting)
+            throws ReporterException {
+        System.out.printf( // NOSONAR
+                "*** itr=%d  size=%d  best_energy=%e  kernel=%s%n",
+                reporting.getIter(),
+                reporting.getMarksAfter().size(),
+                reporting.getMarksAfter().getEnergyTotal(),
+                reporting.kernelDescription());
+    }
+
+    @Override
+    public void aggEnd(Aggregator agg) {
+        // NOTHING TO DO
+    }
+
+    @Override
+    protected AggregateReceiver<VoxelizedMarksWithEnergy> getAggregateReceiver() {
+        return this;
+    }
+
+    @Override
+    public void reportEnd(OptimizationFeedbackEndParams<VoxelizedMarksWithEnergy> optStep) {
+        super.reportEnd(optStep);
+        timer.stop();
+        System.out.printf( // NOSONAR
+                "Optimization time took %e s%n", ((double) timer.getTime()) / 1000);
+    }
+}
