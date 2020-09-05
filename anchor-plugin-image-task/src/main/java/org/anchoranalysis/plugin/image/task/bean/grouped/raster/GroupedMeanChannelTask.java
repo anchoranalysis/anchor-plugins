7bd3cdb1799f5cb5f05e7fcb4e64865e46f57c9e
Owen Feehan <owenfeehan@users.noreply.github.com>
2020-09-05 17:56:53 +0200

    Refactor

--- anchor-plugin-image-task/src/main/java/org/anchoranalysis/plugin/image/task/bean/grouped/raster/GroupedMeanChannelTask.java
+++ anchor-plugin-image-task/src/main/java/org/anchoranalysis/plugin/image/task/bean/grouped/raster/GroupedMeanChannelTask.java
@@ -50,7 +50,7 @@ import org.anchoranalysis.plugin.image.task.grouped.NamedChannel;
  *
  * @author Owen Feehan
  */
-public class GroupedMeanChannelTask extends GroupedStackTask<Channel, AggregateChannel> {
+public class GroupedMeanChannelTask extends GroupedStackTask<Channel, RunningSumChannel> {
 
     // START BEAN PROPERTIES
     /**
@@ -61,7 +61,7 @@ public class GroupedMeanChannelTask extends GroupedStackTask<Channel, AggregateC
     // END BEAN PROPERTIES
 
     @Override
-    protected GroupMapByName<Channel, AggregateChannel> createGroupMap(
+    protected GroupMapByName<Channel, RunningSumChannel> createGroupMap(
             ConsistentChannelChecker channelChecker) {
         return new GroupedMeanChannelMap();
     }
@@ -70,7 +70,7 @@ public class GroupedMeanChannelTask extends GroupedStackTask<Channel, AggregateC
     protected void processKeys(
             NamedStacks store,
             Optional<String> groupName,
-            GroupedSharedState<Channel, AggregateChannel> sharedState,
+            GroupedSharedState<Channel, RunningSumChannel> sharedState,
             BoundIOContext context)
             throws JobExecutionException {
 
