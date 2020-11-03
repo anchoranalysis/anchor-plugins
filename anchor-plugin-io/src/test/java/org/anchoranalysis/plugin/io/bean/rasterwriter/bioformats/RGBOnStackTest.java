481823979d90655acc5a7116568eb194bd3b083a
Owen Feehan <owenfeehan@users.noreply.github.com>
2020-11-03 12:08:05 +0100

    anchoranalysis/anchor#88 fixed, renamed and expanded OMEXMLRGBTest.java

--- anchor-plugin-io/src/test/java/org/anchoranalysis/plugin/io/bean/rasterwriter/bioformats/RGBOnStackTest.java
+++ anchor-plugin-io/src/test/java/org/anchoranalysis/plugin/io/bean/rasterwriter/bioformats/RGBOnStackTest.java
@@ -11,20 +11,38 @@ import org.anchoranalysis.test.image.rasterwriter.StackTester;
 import org.junit.Test;
 
 /**
- * Tests that the RGB flag is set as expected.
+ * Tests that the RGB flag is set as expected after loading from several different formats.
+ * 
  * @author Owen Feehan
  *
  */
-public class OMEXMLRGBTest {
+public class RGBOnStackTest {
+
+    /** A 8-Bit RGB Stack. */
+    private static ChannelSpecification CHANNEL_SPECIFICATION = new ChannelSpecification(UnsignedByteVoxelType.INSTANCE, 3, true);
 
     @Test
-    public void testOpen() {
-        assertTrue( loadStack("ome.xml").isRgb() );
+    public void testOMETiff() {
+        assertForFormat("ome.tif");
+    }
+    
+    @Test
+    public void testTiff() {
+        assertForFormat("tif");
+    }
+    
+    @Test
+    public void testPNG() {
+        assertForFormat("png");
+    }
+    
+    private void assertForFormat(String formatExtension) {
+        assertTrue( loadStack(formatExtension).isRgb() );
     }
     
     private Stack loadStack(String extension) {
         String identifier = IdentifierHelper.identiferFor(
-                new ChannelSpecification(UnsignedByteVoxelType.INSTANCE, 3, true), false, StackTester.EXTENT_IDENTIFIER, false);
+                CHANNEL_SPECIFICATION, false, StackTester.EXTENT_IDENTIFIER, false);
         
         TestLoaderImage loader = new TestLoaderImage( SavedFiles.createLoader(extension) );
         return loader.openStackFromTestPath( identifier + "." + extension);        
