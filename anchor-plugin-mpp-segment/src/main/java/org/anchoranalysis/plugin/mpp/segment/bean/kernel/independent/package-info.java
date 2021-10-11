68de191e27f05ba0a3b07aef965a5887bba816e4
Owen Feehan <owenfeehan@users.noreply.github.com>
2021-10-11 21:05:12 +0200

    Updated javadoc

--- anchor-plugin-mpp-segment/src/main/java/org/anchoranalysis/plugin/mpp/segment/bean/kernel/independent/package-info.java
+++ anchor-plugin-mpp-segment/src/main/java/org/anchoranalysis/plugin/mpp/segment/bean/kernel/independent/package-info.java
@@ -1,6 +1,6 @@
 /**
  * Implementations of {@link org.anchoranalysis.plugin.mpp.segment.bean.kernel.independent.KernelBirth} that are <b>independent</b>.
  * 
- * <p><i>Independent</i> means that the probability of a kernel move is <i>independent</b> of the existing configuration state.
+ * <p><i>Independent</i> means that the probability of a kernel move is <i>independent</i> of the existing configuration state.
  */
 package org.anchoranalysis.plugin.mpp.segment.bean.kernel.independent;
