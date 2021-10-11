e558d1f81f7cb4c5825556ae04deff049e5c3913
Owen Feehan <owenfeehan@users.noreply.github.com>
2021-10-11 09:44:53 +0200

    Added javadoc package-info for anchor-plugin-imagej

---
+++ anchor-plugin-imagej/src/main/java/org/anchoranalysis/plugin/imagej/bean/channel/provider/filter/moments/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Non-linear <a href="https://homepages.inf.ed.ac.uk/rbf/HIPR2/filtops.htm">filters</a> using <a href="https://imagej.net/Welcome">ImageJ</a> that use the <a href="https://en.wikipedia.org/wiki/Moment_(mathematics)">statistical moments</a> of the intensity values in the kernel. 
+ */
+package org.anchoranalysis.plugin.imagej.bean.channel.provider.filter.moments;
