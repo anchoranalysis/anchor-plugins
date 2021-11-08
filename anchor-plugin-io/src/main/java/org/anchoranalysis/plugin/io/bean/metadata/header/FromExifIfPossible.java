9b594ae387322e42bf7fad271d8db5c5a6f83b0f
Owen Feehan <owenfeehan@users.noreply.github.com>
2021-11-08 10:29:17 +0100

    anchor#190 added PNG header support for inferring image metadata.

--- anchor-plugin-io/src/main/java/org/anchoranalysis/plugin/io/bean/metadata/header/FromExifIfPossible.java
+++ anchor-plugin-io/src/main/java/org/anchoranalysis/plugin/io/bean/metadata/header/FromExifIfPossible.java
@@ -1,8 +1,6 @@
-package org.anchoranalysis.plugin.io.bean.metadata.reader;
+package org.anchoranalysis.plugin.io.bean.metadata.header;
 
-import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
-import com.drew.metadata.MetadataException;
 import com.drew.metadata.exif.ExifIFD0Directory;
 import com.drew.metadata.jpeg.JpegDirectory;
 import java.util.Optional;
@@ -16,12 +14,7 @@ import org.anchoranalysis.spatial.box.Extent;
 /**
  * Infers the size of the image from tags in {@link Metadata}.
  *
- * <p>It tries, in this order:
- *
- * <ul>
- *   <li>any EXIF header
- *   <li>any JPEG header
- * </ul>
+ * <p>It tries first to read from any EXIF header, and if that is absent, then a fallback.
  *
  * All are presumes to describe the image <b><i>before</i></b> any {@link OrientationChange} is
  * applied.
@@ -29,7 +22,7 @@ import org.anchoranalysis.spatial.box.Extent;
  * @author Owen Feehan
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-class InferExtentFromMetadata {
+class FromExifIfPossible {
 
     /**
      * Infers the {@link Extent} from the metadata.
@@ -40,12 +33,12 @@ class InferExtentFromMetadata {
      * @return the extent, if it is existed, orientated to match {@code orientation}.
      * @throws ImageIOException if metadata exists in an invalid state.
      */
-    public static Optional<Extent> inferExtent(
+    public static Optional<Extent> inferExtentFromEXIFOr(
             Metadata metadata, Optional<OrientationChange> orientation) throws ImageIOException {
 
         Optional<Extent> extent =
                 OptionalUtilities.orFlatSupplier(
-                        () -> readExif(metadata), () -> readJPEG(metadata));
+                        () -> readExif(metadata), () -> readOther(metadata));
 
         if (extent.isPresent()) {
             if (orientation.isPresent()) {
@@ -59,49 +52,18 @@ class InferExtentFromMetadata {
     }
 
     private static Optional<Extent> readExif(Metadata metadata) throws ImageIOException {
-        return readFromWidthHeightTags(
+        return InferHelper.readFromWidthHeightTags(
                 metadata,
                 ExifIFD0Directory.class,
                 ExifIFD0Directory.TAG_IMAGE_WIDTH,
                 ExifIFD0Directory.TAG_IMAGE_HEIGHT);
     }
 
-    private static Optional<Extent> readJPEG(Metadata metadata) throws ImageIOException {
-        return readFromWidthHeightTags(
+    private static Optional<Extent> readOther(Metadata metadata) throws ImageIOException {
+        return InferHelper.readFromWidthHeightTags(
                 metadata,
                 JpegDirectory.class,
                 JpegDirectory.TAG_IMAGE_WIDTH,
                 JpegDirectory.TAG_IMAGE_HEIGHT);
-    }
-
-    private static <T extends Directory> Optional<Extent> readFromWidthHeightTags(
-            Metadata metadata, Class<T> directoryType, int tagWidth, int tagHeight)
-            throws ImageIOException {
-
-        Directory directory = metadata.getFirstDirectoryOfType(directoryType);
-
-        // Search for a width and height directly in the EXIF
-        // It is assumed this
-        if (directory != null
-                && directory.containsTag(tagWidth)
-                && directory.containsTag(tagHeight)) {
-
-            try {
-                int width = directory.getInt(tagWidth);
-                int height = directory.getInt(tagHeight);
-
-                if (width == 0 || height == 0) {
-                    throw new ImageIOException(
-                            "A width or height of 0 was specified in metadata, which suggests a format is not supported e.g perhaps JPEGs with DNL markers.");
-                }
-                return Optional.of(new Extent(width, height));
-
-            } catch (MetadataException e) {
-                throw new ImageIOException("Image metadata exists in an invalid state.", e);
-            }
-
-        } else {
-            return Optional.empty();
-        }
     }
 }
