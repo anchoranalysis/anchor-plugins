5556ffae0d729dfb1cf63b980673589dc9e9c3de
Owen Feehan <owenfeehan@users.noreply.github.com>
2025-05-12 19:10:12 +0200

    Add metadata features for GPS coordinates and extraction time; add license to DateTimeAssociator.java

---
+++ anchor-plugin-image-feature/src/main/java/org/anchoranalysis/plugin/image/feature/bean/metadata/CreationTime.java
@@ -0,0 +1,20 @@
+package org.anchoranalysis.plugin.image.feature.bean.metadata;
+
+import java.time.ZonedDateTime;
+import java.util.Optional;
+import org.anchoranalysis.image.core.stack.ImageMetadata;
+
+/**
+ * The creation-time of the image, if specified on the image file.
+ *
+ * <p>It is specified as the number of seconds from the epoch of 1970-01-01T00:00:00Z.
+ *
+ * <p>If the creation-time is not specified, this feature will return NaN.
+ */
+public class CreationTime extends ExtractTime {
+
+    @Override
+    protected Optional<ZonedDateTime> extractTime(ImageMetadata metadata) {
+        return Optional.of(metadata.getFileAttributes().getCreationTime());
+    }
+}
